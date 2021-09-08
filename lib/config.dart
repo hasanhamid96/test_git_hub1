18440e7ef2068e930078de732add942cd6eedb4f
hasan hamid <hassan96hamid@gmail.com>
2021-09-08 13:15:24 +0300

    agora

---
+++ lib/config.dart
@@ -0,0 +1,14 @@
+/// Get your own App ID at https://dashboard.agora.io/
+const appId ='b5372d83e8e64247b54c73394373bf95';
+
+/// Please refer to https://docs.agora.io/en/Agora%20Platform/token
+const token = '006b5372d83e8e64247b54c73394373bf95IADag8v+BlvTwc2zIajAN9cbb7wZ57MOyy0TrDXEi/BqGbCTz/IAAAAAEAAY8Zr82ec5YQEAAQDZ5zlh';
+
+/// Your channel ID
+const channelId = 'YOUR_CHANNEL_ID';
+
+/// Your int user ID
+const uid = 1234543;
+
+/// Your string user ID
+const stringUid = 'wefknekfnm';
